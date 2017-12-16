9aaa0deee5fb4da67c4779e7f77c377ec8ce18ed
yuxu <yuxu@github.com>
2017-12-16 23:48:50 +0100

    ft_ERRORS DONE

--- ft_errors.c
+++ ft_errors.c
@@ -28,8 +28,8 @@ int		four_x_four(char **board)
 			return (0);
 		line++;
 	}
-	if (*board[4] != '\n')
-		return (0);
+//	if (*board[4] != '\n')
+//		return (0);  to be deleted since we have 4 lines (or 3 if you count from 0)
 	return (1);
 }
 
@@ -61,6 +61,22 @@ int		input_check(char **board)
 	return (1);
 }
 
+int		c_count(char **board, int line, int i)
+{
+	int connect_count;
+
+	connect_count = 0;
+	if (line <= 3 && board[line + 1][i] == '#')
+		connect_count++;
+	if (line >= 1 && board[line - 1][i] == '#')
+		connect_count++;
+	if (i <= 2 && board[line][i + 1] == '#')
+		connect_count++;
+	if (i >= 1 && board[line][i - 1] == '#')
+		connect_count++;
+	return (connect_count);
+}
+
 int		hashtag_test(char **board)
 {
 	int i;
@@ -75,21 +91,12 @@ int		hashtag_test(char **board)
 		while (i <= 3)
 		{
 			if (board[line][i] == '#')
-			{
-				if (line <= 3 && board[line + 1][i] == '#')
-					connect_count++;
-				if (line >= 1 && board[line - 1][i] == '#')
-					connect_count++;
-				if (i <= 2 && board[line][i + 1] == '#')
-					connect_count++;
-				if (i >= 1 && board[line][i - 1] == '#')
-					connect_count++;
-			}
+				connect_count += c_count(board, line, i);
 			i++;
 		}
 		line++;
 	}
-	printf("connections\(if pass the other two tests): %d\n", connect_count);
+//	printf("connections\(if pass the other two tests): %d\n", connect_count);
 	if (connect_count != 6 && connect_count != 8)
 		return (0);
 	return (1);
@@ -102,17 +109,18 @@ int		ft_errors(char **one_board)
 		return (0);
 	return (1);
 }
-
+/*
 int		main(void)
 {
 	int i;
-	char *str1 = "....\n .##.\n .##.\n ....\n \n"; //ok
+	char *str1 = "....\n .##.\n .##.\n ...."; //ok
 	char *str2 = "...#\n #...\n ...#\n #...\n \n"; //no ok
 	char *str3 = "####\n ....\n ....\n ....\n \n"; //ok
 	char *str4 = "....\n ....\n ....\n ####\n \n"; //ok
 	char *str5 = "....\n ....\n ..#.\n .###\n \n"; //ok
-	char *str6 = "..#.\n ....\n ..##\n ....\n \n"; // ok
+	char *str6 = "..#.\n ....\n ..##\n ....\n \n"; //no ok
 	char *str7 = "..#.\n ..#.\n ..##\n ....\n \n"; // ok
+	char *str8 = "....\n ....\n ..##\n .##.\n \n"; //no ok
 
 char	**test1 = ft_strsplit(str1, ' '); // change number here
 char	**test2 = ft_strsplit(str2, ' '); // change number here
@@ -121,6 +129,8 @@ char	**test4 = ft_strsplit(str4, ' '); // change number here
 char	**test5 = ft_strsplit(str5, ' '); // change number here
 char	**test6 = ft_strsplit(str6, ' '); // change number here
 char	**test7 = ft_strsplit(str7, ' '); // change number here
+char	**test8 = ft_strsplit(str8, ' '); // change number here
+
 	i = ft_errors(test1);
 	printf("result1: %d\n", i);
 	i = ft_errors(test2);
@@ -135,7 +145,8 @@ char	**test7 = ft_strsplit(str7, ' '); // change number here
 	printf("result6: %d\n", i);
 	i = ft_errors(test7);
 	printf("result7: %d\n", i);
-
+	i = ft_errors(test8);
+	printf("result8: %d\n", i);
 //	int n = 0;
 //	while (n < 5)
 //	{
@@ -159,4 +170,4 @@ char	**test7 = ft_strsplit(str7, ' '); // change number here
 //		table_nb++;
 //	}
 //	return (1);
-//}
+//}*/
