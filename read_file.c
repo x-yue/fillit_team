36094205c4972241c84c380e5d9c5000d5e0b9e1
Andréas BLIN <ablin@mbp-de-andreas.home>
2017-12-19 18:06:28 +0100

    fixed read_file pb

--- read_file.c
+++ read_file.c
@@ -10,17 +10,12 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include "libft.h"
 #include "fillit.h"
 #include "ft_errors.c"
 #include <stdio.h>
 #include "T1.c"
 
-char	ft_checksize(char *str)
+int	ft_checksize(char *str)
 {
 	int		i;
 
@@ -41,7 +36,6 @@ void	ft_show(char **arr)
 	int j;
 
 	i = 0;
-	ft_putstr("BOUCLE\n");
 	while (arr[i] != NULL)
 	{
 		j = 0;
@@ -54,15 +48,7 @@ void	ft_show(char **arr)
 	}
 }
 
-void	ft_sendstring(char **board)
-{
-	board[4] = NULL;
-	ft_show(board); //change this
-	free(board);// is this necessary?
-	board = NULL;// is this?
-}
-
-char	ft_cutstring(char *str, int i)
+int	ft_cutstring(char *str, int i)
 {
 	i_list	list;
 	char	**board;
@@ -84,13 +70,16 @@ char	ft_cutstring(char *str, int i)
 		board[list.j][list.k] = '\0';
 		list.j++;
 	}
-	ft_sendstring(board);
-	if (str[i] == '\n')
+	i++;
+	board[4] = NULL;
+	if (ft_first_tetri(board) == 0)//change this
+		return (0);
+	if (str[i] != '\0')
 		ft_cutstring(str, i);
 	return (1);
 }
 
-char	ft_newstring(int size, char *filename)
+int	ft_newstring(int size, char *filename)
 {
 	f_list	list;
 	char	*str;
@@ -98,9 +87,9 @@ char	ft_newstring(int size, char *filename)
 	list.i = 0;
 	if ((list.fd = open(filename, O_RDONLY)) == -1)
 		return (0);
-	if ((str = (char*)malloc(sizeof(char) * (size + 1))) == NULL)
+	if ((str = (char*)malloc(sizeof(char) * (size + 2))) == NULL)
 		return (0);
-	ft_bzero(str, size + 1);
+	ft_bzero(str, size + 2);
 	while ((list.rd = read(list.fd, list.buf, 1)) != 0)
 	{
 		str[list.i] = *list.buf;
@@ -111,15 +100,14 @@ char	ft_newstring(int size, char *filename)
 		return (0);
 	if (ft_checksize(str) == 0)
 		return (0);
-//	ft_putstr(str);
-//	printf("%d\n", (int)ft_strlen(str));
-	ft_cutstring(str, 0);
+	if (ft_cutstring(str, 0) == 0)
+		return (0);
 	if (close(list.fd) == -1)
 		return (0);
 	return (1);
 }
 
-char	ft_read(char *filename)
+int	ft_read(char *filename)
 {
 	f_list	list;
 
@@ -128,8 +116,8 @@ char	ft_read(char *filename)
 		ft_putstr("error");
 		return (0);
 	}
-	list.rd = read(list.fd, list.buf, BUF_SIZE);
-	if (list.rd == -1)
+	list.rd += read(list.fd, list.buf, BUF_SIZE);
+	if (list.rd == -1 || list.rd > BUF_SIZE)
 	{
 		ft_putstr("error");
 		return (0);
