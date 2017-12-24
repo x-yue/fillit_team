2885608148053c5fa5a19092a172cd9394995296
Andreas Blin <ablin@student.42.fr>
2017-12-24 16:25:44 +0100

    ft_map, expand the map if a tetriminos couldnt fit

---
+++ tetri.c
@@ -0,0 +1,141 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   T1.c                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ablin <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/17 03:02:55 by ablin             #+#    #+#             */
+/*   Updated: 2017/12/18 21:44:50 by ablin            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fillit.h"
+#include "libft.h"
+#include <stdio.h>
+
+void	ft_show1(char **arr)
+{
+	int i;
+	int j;
+	i = 0;
+	while (arr[i] != NULL)
+	{
+		j = 0;
+		while (arr[i][j] != '\0')
+		{
+			ft_putchar(arr[i][j]);
+			j++;
+		}
+		i++;
+	}
+}
+
+char	**ft_set_map(char **map, char **board, int line)
+{
+	int		i;
+	int		j;
+
+	i = 0;
+	while (i < line)
+	{
+		j = 0;
+		while (j < line)
+		{
+			map[i][j] = board[i][j];
+			j++;
+		}
+		map[i][j] = '\n';
+		map[i][j + 1] = '\0';
+		i++;
+	}
+	map[i] = NULL;
+	ft_show1(map);
+	return (map);
+}
+
+char	**ft_first_map(int line, int col, char **board)
+{
+	char	**map;
+	int		i;
+
+	i = 0;
+	if (line < col)
+		line = col;
+	if ((map = (char **)malloc(sizeof(char *) * (line + 1))) == NULL)
+		return (0);
+	while (i < line)
+	{
+		if ((map[i] = (char *)malloc(sizeof(char) * (line + 1))) == NULL)
+			return (0);
+		i++;
+	}
+	map = ft_set_map(map, board, line);
+	return (map);
+}
+
+int		ft_first_tetri(char **board)
+{
+	tetri_struct	tetri;
+
+	tetri.i = 0;
+	tetri.column = 0;
+	tetri.total = 0;
+	while (board[tetri.i] != NULL && tetri.total != 4)
+	{
+		tetri.count = 0;
+		tetri.j = 0;
+		while (board[tetri.i][tetri.j] != '\0')
+		{
+			if (board[tetri.i][tetri.j] == '#')
+			{
+				board[tetri.i][tetri.j] = 'A';
+				tetri.count = tetri.j + 1;
+				tetri.total++;
+			}
+			tetri.j++;
+		}
+		if (tetri.column < tetri.count)
+			tetri.column = tetri.count;
+		tetri.i++;
+	}
+	ft_first_map(tetri.i, tetri.column, board);
+	return (1);
+}
+
+char	**ft_map(char **map, int size)
+{
+	char	**newmap;
+	int		line;
+	int		col;
+
+	if ((newmap = (char **)malloc(sizeof(char *) * (size + 2))) == NULL)//size + 2 cause we send the size of the board w/o counting the \0
+		return (0);
+	line = 0;
+	while (map[line] != NULL)
+	{
+		col = -1;
+		newmap[line] = ft_strnew(size + 2);//see above why size + 2
+		while (map[line][++col] != '\n')
+			newmap[line][col] = map[line][col];
+		newmap[line][col] = '.';
+		newmap[line][col + 1] = '\n';
+		newmap[line][col + 2] = '\0';
+		line++;
+	}
+	col = -1;
+	newmap[line] = ft_strnew(size + 1);
+	while (col++ < size)
+		newmap[line][col] = '.';
+	newmap[line][col] = '\n';
+	newmap[line][col + 1] = '\0';
+	newmap[line + 1] = NULL;
+	ft_show1(newmap);
+	return (newmap);
+}
+/*
+int	main()
+{
+	ft_first_tetri(ft_strsplit(".#..\n ###.\n ....\n ....\n ", ' '));
+	return (0);
+}*/
