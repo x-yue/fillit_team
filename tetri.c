a2aa29147ebe8acef085f385f255d2ffe2598dcf
Andreas Blin <ablin@student.42.fr>
2018-01-04 19:24:20 +0100

    need to assemble it all together

--- tetri.c
+++ tetri.c
@@ -31,76 +31,36 @@ void	ft_show1(char **arr)
 	}
 }
 
-char	**ft_set_map(char **map, char **board, int line)
+char	**ft_map(char **map, int size)//should free the old map
 {
-	int		i;
-	int		j;
+	char	**newmap;
+	int		line;
+	int		col;
 
-	i = 0;
-	while (i < line)
-	{
-		j = 0;
-		while (j < line)
-		{
-			map[i][j] = board[i][j];
-			j++;
-		}
-		map[i][j] = '\n';
-		map[i][j + 1] = '\0';
-		i++;
-	}
-	map[i] = NULL;
-	ft_show1(map);
-	return (map);
-}
-
-char	**ft_first_map(int line, int col, char **board)
-{
-	char	**map;
-	int		i;
-
-	i = 0;
-	if (line < col)
-		line = col;
-	if ((map = (char **)malloc(sizeof(char *) * (line + 1))) == NULL)
+	if ((newmap = (char **)malloc(sizeof(char *) * (size + 2))) == NULL)//size + 2 cause we send the size of the board w/o counting the \0
 		return (0);
-	while (i < line)
+	line = 0;
+	while (map[line] != NULL)
 	{
-		if ((map[i] = (char *)malloc(sizeof(char) * (line + 1))) == NULL)
-			return (0);
-		i++;
+		col = -1;
+		newmap[line] = ft_strnew(size + 2);//see above why size + 2
+		while (map[line][++col] != '\n')
+			newmap[line][col] = map[line][col];
+		newmap[line][col] = '.';
+		newmap[line][col + 1] = '\n';
+		newmap[line][col + 2] = '\0';
+		line++;
 	}
-	map = ft_set_map(map, board, line);
-	return (map);
-}
-
-int		ft_first_tetri(char **board)
-{
-	tetri_struct	tetri;
-
-	tetri.i = 0;
-	tetri.column = 0;
-	tetri.total = 0;
-	while (board[tetri.i] != NULL && tetri.total != 4)
-	{
-		tetri.count = 0;
-		tetri.j = 0;
-		while (board[tetri.i][tetri.j] != '\0')
-		{
-			if (board[tetri.i][tetri.j] == '#')
-			{
-				board[tetri.i][tetri.j] = 'A';
-				tetri.count = tetri.j + 1;
-				tetri.total++;
-			}
-			tetri.j++;
-		}
-		if (tetri.column < tetri.count)
-			tetri.column = tetri.count;
-		tetri.i++;
-	}
-	ft_first_map(tetri.i, tetri.column, board);
-	return (1);
+	col = -1;
+	newmap[line] = ft_strnew(size + 1);
+	while (col++ < size)
+		newmap[line][col] = '.';
+	newmap[line][col] = '\n';
+	newmap[line][col + 1] = '\0';
+	newmap[line + 1] = NULL;
+	ft_show1(newmap);
+	ft_putchar('\n');
+	return (newmap);
 }
 
 char	**ft_set()
@@ -200,12 +160,14 @@ int		ft_fit(char **map, char **board, int size)
 			{
 				pos = ft_getpos(board, call);
 				call++;
-			/////////	printf("(%d : %d)\n", line + pos.x, col + pos.y);
+				//printf("[%d : %d]", line, col);
+				//printf("(%d : %d)\n", line + pos.x, col + pos.y);
 				while ((line + pos.x) < size && (col + pos.y) < size &&  map[line + pos.x][col + pos.y] == '.' && call != 4)
 				{
 					pos = ft_getpos(board, call);
 					call++;
 				}
+					pos = ft_getpos(board, call);
 				if ((line + pos.x) < size && (col + pos.y) < size && map[line + pos.x][col + pos.y] == '.' && call == 4)
 				{
 				ft_insert(map, board, line, col);
@@ -216,40 +178,13 @@ int		ft_fit(char **map, char **board, int size)
 		}
 		line++;
 	}
+	map = ft_map(map, size);
+	ft_show1(map);
+	size++;
+	ft_fit(map, board, size);
 	return (0);
 }
 
-char	**ft_map(char **map, int size)//should free the old map
-{
-	char	**newmap;
-	int		line;
-	int		col;
-
-	if ((newmap = (char **)malloc(sizeof(char *) * (size + 2))) == NULL)//size + 2 cause we send the size of the board w/o counting the \0
-		return (0);
-	line = 0;
-	while (map[line] != NULL)
-	{
-		col = -1;
-		newmap[line] = ft_strnew(size + 2);//see above why size + 2
-		while (map[line][++col] != '\n')
-			newmap[line][col] = map[line][col];
-		newmap[line][col] = '.';
-		newmap[line][col + 1] = '\n';
-		newmap[line][col + 2] = '\0';
-		line++;
-	}
-	col = -1;
-	newmap[line] = ft_strnew(size + 1);
-	while (col++ < size)
-		newmap[line][col] = '.';
-	newmap[line][col] = '\n';
-	newmap[line][col + 1] = '\0';
-	newmap[line + 1] = NULL;
-	ft_show1(newmap);
-	ft_putchar('\n');
-	return (newmap);
-}
 
 void	ft_test2(char **board)
 {
@@ -265,7 +200,7 @@ void	ft_test(char **board)
 	(void)board;
 	char **map;
 
-	map = ft_strsplit("....\n ....\n ....\n ....\n ", ' ');
+	map = ft_strsplit("#...\n #...\n #...\n #...\n ", ' ');
 	ft_putstr("original map\n");
 	ft_show1(map);
 	if (ft_fit(map, board, 4) == 0)
